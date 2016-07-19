f2bfc219cbef99a4c187b1104952ff337af3859f
tawshif <anikphp@gmail.com>
2016-07-20 00:26:15 +0600

    first uploading git from local

---
+++ Gruntfile.js
@@ -0,0 +1,5 @@
+module.exports = function(grunt) {
+    grunt.registerTask('task', function() {
+        console.log('I am here and i am ran by grunt');
+    });
+}
---
+++ node3.js
@@ -0,0 +1,18 @@
+'use strict';
+
+module.exports = {
+
+    song: function() {
+        console.log("this is the song Function");
+    },
+
+    movie: function() {
+        console.log("this is the movie function");
+    },
+
+    fevMovie: function() {
+        console.log("fev movies")
+    }
+
+
+}
