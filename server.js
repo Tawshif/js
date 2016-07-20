fa90371309dd3110a674615b0cd34f1c36451298
Tawshif Ahsan Khan <tawshif.ahsan@sekai-lab-bd.com>
2016-07-20 18:41:55 +0600

    new things

--- server.js
+++ server.js
@@ -1,11 +1,23 @@
 var http = require('http');
+var fs = require('fs');
 
-function onRequest (request, response) {
-	   
-	   console.log("A user made a request" + request.url);
-	   response.writeHead(200, {"Context-type":"text/plain"});
-	   response.write("Here is some data");
-	   response.end();
+// 404 response
+
+function send404Response(response) {
+    response.writeHead(404, { "content-type": "text/plain" });
+    response.write("Error 404 : Page not found !");
+    response.end();
+}
+
+// handle a user request
+function onRequest(request, response) {
+
+    if (request.method == 'GET' && request.url == '/') {
+        response.writeHead(200, { "Content-Type": "text/html" });
+        fs.createReadStream("./index2.html").pipe(response);
+    } else {
+        send404Response(response);
+    }
 }
 
 http.createServer(onRequest).listen(8888);
