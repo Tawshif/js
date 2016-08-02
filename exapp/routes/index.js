522940731f2fdc4ca0a68b8cb1a8a12c6651c334
Tawshif Ahsan Khan <tawshif.ahsan@sekai-lab-bd.com>
2016-08-02 11:45:56 +0600

    changes

--- exapp/routes/index.js
+++ exapp/routes/index.js
@@ -1,13 +1,13 @@
 var express = require('express');
 var router = express.Router();
-
+var vd = require('../videodata.json');
 /* GET home page. */
 router.get('/', function(req, res, next) {
     res.render('index', {
 
         title: 'Express Test',
-        name: 'Tawshif Ahsan'
-
+        name: 'Tawshif Ahsan',
+        videodata: vd
     });
 
 });
